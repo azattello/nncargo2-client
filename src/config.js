b2f0c5ebb702ba4c65f481ffdee2b22e072e96c7
Abdykali Azat <azat.abdykalli@gmail.com>
2024-07-13 20:31:34 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://nncargo.kz:3001',
+    
+    nameCargo: 'NN CARGO'
+  };
+  
+  
+export default config;
+  
